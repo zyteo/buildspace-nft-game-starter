702c31466e1d7fe845895321317669b8638cb128
Stabilo <zteo015@e.ntu.edu.sg>
2021-11-23 12:11:39 +0800

    Set up game logic, added styles

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x478F6bA3d134E759B52679059460ba617D9F7Ac2';
+
+const transformCharacterData = (characterData) => {
+    return {
+      name: characterData.name,
+      imageURI: characterData.imageURI,
+      hp: characterData.hp.toNumber(),
+      maxHp: characterData.maxHp.toNumber(),
+      attackDamage: characterData.attackDamage.toNumber(),
+    };
+  };
+
+export { CONTRACT_ADDRESS,transformCharacterData };
